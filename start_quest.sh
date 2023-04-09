383ae01b6fca37ce0d6e2ad0c36ee2af738de8be
WilcoApp <hello@wilco.gg>
2023-04-09 20:37:02 +0000

    Dataseeding

--- start_quest.sh
+++ start_quest.sh
@@ -4,10 +4,20 @@ API_BASE_URL="${API_BASE_URL:="https://engine.wilco.gg"}"
 WILCO_ID="`cat .wilco 2> /dev/null`"
 
 sendEvent() {
-  error=${2//$'\n'/\\n}
-  body='{"event":"start_quest_script_ran", "metadata": {"success": '$1', "remote": false, "error": "'$error'"}}'
-  curl -m 5 -X POST $API_BASE_URL/users/$WILCO_ID/event -d "$body" -H 'Content-Type: application/json' &> /dev/null
+  curl -m 5 -X POST $API_BASE_URL/users/$WILCO_ID/event -d "$1" -H 'Content-Type: application/json' &> /dev/null
 }
+
+sendScriptRunningEvent() {
+  body='{"event":"start_quest_script_running", "metadata": {"remote": false}}'
+  sendEvent "$body"
+}
+
+sendScriptRanEvent() {
+  body='{"event":"start_quest_script_ran", "metadata": {"success": '$1', "remote": false, "error": "'$2'"}}'
+  sendEvent "$body"
+}
+
+sendScriptRunningEvent
 
 if [[ -z $WILCO_ID ]]; then
   printf "Please run the script in the root folder of the repo. Aborting.\n"
@@ -18,17 +28,17 @@ printf "[=  ] Stashing any existing changes ... \n"
 git stash &> /dev/null
 
 printf "[== ] Resetting local 'main' branch to 'origin/main' ... \n"
-git_output=$((git checkout main && git fetch origin main && git reset --hard origin/main) 2>&1 > /dev/null)
+git_output=$( (git checkout main && git fetch origin main && git reset --hard origin/main) 2>&1 > /dev/null)
 git_exit_code=$?
 
 if ! test "$git_exit_code" -eq 0
 then
    printf "[==X] ERROR: Could not reset local 'main' branch. Aborting.\n"
-   sendEvent false "$git_output"
+   sendScriptRanEvent false "$git_output"
    echo "[==X] GIT ERROR: $git_output"
    exit 1
 fi
 
 printf "[===] SUCCESS: Your local 'main' branch is now up to date with origin.\n"
-sendEvent true
+sendScriptRanEvent true
 exit 0
