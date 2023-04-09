1f5af2fe765cfe93446e114e2f4cc4b98b0376a5
Eduardo Paredes <ed.paredes@gmail.com>
2023-04-10 01:08:30 +0200

    fix: correct seed script

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,35 +1,53 @@
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+import os
 
-from repositories.users import UsersRepository
-from repositories.items import ItemsRepository
-from repositories.comments import CommentsRepository
-import random
-import string
-from core.config import get_app_settings
+database_url = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://")
 
-
-database_url = get_app_settings().database_url.replace("postgres://", "postgresql://")
-
+user_insert_statement = text(
+    """INSERT INTO users(username, email, salt, bio, hashed_password) VALUES(:username, :email, :salt, :bio, :hashed_password)"""
+)
+select_last_user_id = text("""SELECT * FROM users ORDER BY id DESC LIMIT 1""")
+item_statement = text(
+    """INSERT INTO items(slug, title, description, seller_id) VALUES(:slug, :title, :description, :seller_id)"""
+)
+select_last_item_id = text("""SELECT * FROM items ORDER BY id DESC LIMIT 1""")
+comment_statement = text(
+    """INSERT INTO comments(body, seller_id, item_id) VALUES(:body, :seller_id, :item_id)"""
+)
 
 engine = create_engine(database_url, echo=True)
-letters = string.ascii_lowercase
+
 with engine.connect() as connect:
-    user_repository = UsersRepository(connect)
-    items_repository = ItemsRepository(connect)
-    comments_repository = CommentsRepository(connect)
-    
     for i in range(100):
-        random_username = ''.join(random.choice(letters) for i in range(24))
-        user = user_repository.create_user(username=random.choice(),email= f'{random_username}@mail.com',password= 'super_secure')
+        random_user = f'user_{i}'
+        user = {
+            "username": random_user,
+            "email": f"{random_user}@mail.com",
+            "salt": "abc_123",
+            "bio": f"{random_user} bio",
+            "hashed_password": f"super_secuere_{i}"
+        }
+        connect.execute(user_insert_statement, **user)
+        result = connect.execute(select_last_user_id)
         
-        item = items_repository.create_item(
-            slug=f'slug-{random_username}',
-            title=f'Title-{i}',
-            description=f'Description-{i}',
-            seller=user,
-            body=f'Body {i}',
-            image=f'image_{i}',
-            tags=f'Tags--->{i}',
-        )
-        comments = comments_repository.create_comment_for_item(body=f'body---{i}', item=item, user=user)
-    
+        result = connect.execute(select_last_user_id).one()
+        generated_user_id = result.id
+
+        item = {
+            "slug": f"slug-{random_user}",
+            "title": f"title{i}",
+            "description": f"desc{i}",
+            "seller_id": generated_user_id,
+        }
+        connect.execute(item_statement, **item)
+
+        item_result = connect.execute(select_last_item_id).one()
+        generated_item_id = item_result.id
+        
+        comment = {
+            "body": f"comment{i}",
+            "seller_id": generated_user_id,
+            "item_id": generated_item_id,
+        }
+        connect.execute(comment_statement, **comment)
